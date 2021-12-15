6ad1d9da7b8b8c26b4b427e8e2f824b90a16c29c
Hiroto <hiro.f7610@gmail.com>
2021-12-15 16:39:11 +0900

    Create Framework of TeamPage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+react-router-dom v6 から、講義中のバージョンとは以下の箇所が異なる
+1. "Router"→"BrowserRouter"
+2. "Switch"→"Routes"
+3. "exact path"→"path"
+4. "component={ コンポーネント名 }"→"element={ < コンポーネント名 /> }"
+*/
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { MembersPage } from "./frontend-page/MembersPage";
+import React from "react";
+import { TeamPage } from "./frontend-page/TeamPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <TeamPage /> } />
+        <Route path='/members' element={ <MembersPage/> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
